e013ae91d68db7b5e17969e5250fc49e11edf468
Chris Ruffalo <chris.ruffalo@gmail.com>
2020-08-18 11:40:30 -0400

    added version moved workflows

---
+++ version/version.go
@@ -0,0 +1,64 @@
+package version
+
+import (
+	"strings"
+)
+
+// pick up version from build process, but use these defaults
+var Version = "v0.1.X"
+var Release = "1"
+var GitHash = "0000000"
+var LongVersion = ""
+var Descriptor = ""
+
+type VersionInfo struct {
+	Version     string
+	LongVersion string
+	Release     string
+	GitHash     string
+}
+
+func GetVersion() string {
+	return strings.TrimSpace(Version)
+}
+
+func GetLongVersion() string {
+	// allow LongVersion override
+	if "" != LongVersion {
+		return LongVersion
+	}
+	// add git hash to long version if available
+	version := GetVersion()
+	if "" != GetRelease() {
+		version = version + "-" + GetRelease()
+	}
+	if "" != GetGitHash() {
+		version = version + "-git@" + GetGitHash()
+	}
+	if "" != GetDescriptor() {
+		version = version + " (" + GetDescriptor() + ")"
+	}
+
+	return strings.TrimSpace(version)
+}
+
+func GetGitHash() string {
+	return strings.TrimSpace(GitHash)
+}
+
+func GetRelease() string {
+	return strings.TrimSpace(Release)
+}
+
+func GetDescriptor() string {
+	return strings.TrimSpace(Descriptor)
+}
+
+func Info() VersionInfo {
+	return VersionInfo{
+		Version:     GetVersion(),
+		LongVersion: GetLongVersion(),
+		Release:     GetRelease(),
+		GitHash:     GetGitHash(),
+	}
+}
